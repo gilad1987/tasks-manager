bfc57757bb44df604713dedf4eebcbebc05a86eb
gilad1987 <gilad1987@gmail.com>
2015-02-22 14:21:40 -0800

    add gt-tasks-manager directive

--- src/js/gt-tasks-manager/gt-task.drv.js
+++ src/js/gt-tasks-manager/gt-task.drv.js
@@ -1,6 +1,7 @@
 
 (function (angular) {
 
+
     function GtTaskDirective(RecursionHelper)
     {
 
@@ -13,6 +14,9 @@
 
             },
             compile: function(element) {
+                element.css('background','#' + (function co(lor){   return (lor +=
+                    [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
+                && (lor.length == 6) ?  lor : co(lor); })(''));
                 return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){
 
                     // Define your normal link function here.
